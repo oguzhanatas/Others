6a45783334df06ba2c9289da857a06596f4103cf
Oguz Han <oguz.atas@ogr.iu.edu.tr>
2021-06-21 21:13:21 +0300

    update

--- rotationPoints_animate.py
+++ rotationPoints_animate.py
@@ -30,16 +30,16 @@ ax.set_yticklabels([0.45*150,0.72*150,150],fontsize=7)
 #ax.scatter(r,t,c='y',s=15,zorder=1)
 line0, = ax.plot(r, 0., color ='y', marker = 'o', markersize = 10, zorder=1)#star location
 ax.text(-1,15,'Sun',color='orange',fontsize=10,zorder=2)
-line1, = ax.plot([0, 0],[0,t], 'bo', linewidth = 1, markersize=1*psize, zorder=0)#earth distance from sun
-line2, = ax.plot([0, 0],[0,t*0.72], 'go', linewidth = 1, markersize=0.949*psize, zorder=0)#venus distance from sun
-line3, = ax.plot([0, 0],[0,t*0.45], 'ro', linewidth = 1, markersize=0.383*psize, zorder=0)#mercury distance from sun
+line1, = ax.plot([],[], 'bo', linewidth = 1, markersize=1*psize, zorder=0)#earth distance from sun
+line2, = ax.plot([],[], 'go', linewidth = 1, markersize=0.949*psize, zorder=0)#venus distance from sun
+line3, = ax.plot([],[], 'ro', linewidth = 1, markersize=0.383*psize, zorder=0)#mercury distance from sun
 
 
 def update(angle):
     #print(angle)
-    line1.set_data([angle, angle],[0,150])#[0,t] where '0' is focus point and 't' is animating point
-    line2.set_data([angle, angle+1],[0,150*0.72])
-    line3.set_data([angle, angle+1.5],[0,150*0.45])
+    line1.set_data([angle, angle*1.],[0,150])#[0,t] where '0' is center point and 't' is distance from center
+    line2.set_data([angle, angle*1.62],[0,150*0.72])
+    line3.set_data([angle, angle*4.14],[0,150*0.45])
     #line0.set_alpha(0.5)
     #line0.set_zorder('1')
     return line0, line1, line2, line3
@@ -47,6 +47,6 @@ def update(angle):
 frames = np.linspace(0,2*np.pi,120)
 
 #fig.canvas.draw()
-ani = matplotlib.animation.FuncAnimation(fig, update, frames=frames, blit=True, interval=15)
+ani = matplotlib.animation.FuncAnimation(fig, update, frames=frames, blit=True, interval=10)
 ani.save('./fig_output/'+'rotationPoints_animate.gif', writer='PillowWriter', fps=30)
 plt.show()
