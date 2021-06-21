f969e6e5a8f92300ed2a485f9ad87227c34f02d0
Oguz Han <oguz.atas@ogr.iu.edu.tr>
2021-06-21 17:50:44 +0300

    update

--- rotationPoints_animate.py
+++ rotationPoints_animate.py
@@ -14,7 +14,8 @@ import matplotlib.pyplot as plt
 import matplotlib.animation
 
 r = 90  * (np.pi/180)
-t = 50#distance
+t = 150#distance
+psize = 10#radius ratio
 
 fig = plt.figure()
 ax = fig.gca(projection = 'polar')
@@ -24,17 +25,19 @@ ax.set_theta_zero_location('N')
 ax.set_theta_direction(-1)
 ax.set_ylim(0,1.02*t)
 #ax.scatter(r,t,c='y',s=15,zorder=1)
-line0, =ax.plot(r, 0., color ='y', marker = 'o', markersize = '20', zorder=1)#star location
-line1, = ax.plot([0, 0],[0,t], 'bo', linewidth = 1, markersize=5, zorder=0)#planet1 location
-line2, = ax.plot([0, 0],[0,t-10], 'go', linewidth = 1, markersize=4, zorder=0)#planet2 location
-line3, = ax.plot([0, 0],[0,t-20], 'ro', linewidth = 1, markersize=4, zorder=0)#planet3 location
+line0, = ax.plot(r, 0., color ='y', marker = 'o', markersize = 10, zorder=1)#star location
+ax.text(-1,15,'Sun',color='orange',fontsize=10,zorder=2)
+line1, = ax.plot([0, 0],[0,t], 'bo', linewidth = 1, markersize=1*psize, zorder=0)#earth distance from sun
+line2, = ax.plot([0, 0],[0,t*0.72], 'go', linewidth = 1, markersize=0.949*psize, zorder=0)#venus distance from sun
+line3, = ax.plot([0, 0],[0,t*0.45], 'ro', linewidth = 1, markersize=0.383*psize, zorder=0)#mercury distance from sun
 
 
 def update(angle):
     #print(angle)
     line1.set_data([angle, angle],[0,t])#[0,t] where '0' is focus point and 't' is animating point
-    line2.set_data([angle, angle+1],[0,t-10])
-    line3.set_data([angle, angle+1.5],[0,t-20])
+    line2.set_data([angle, angle+1],[0,t*0.72])
+    line3.set_data([angle, angle+1.5],[0,t*0.45])
+    #line0.set_alpha(0.5)
     line0.set_zorder('1')
     line1.set_zorder('0')
     line2.set_zorder('0')
@@ -44,6 +47,6 @@ def update(angle):
 frames = np.linspace(0,2*np.pi,120)
 
 fig.canvas.draw()
-ani = matplotlib.animation.FuncAnimation(fig, update, frames=frames, blit=True, interval=10)
+ani = matplotlib.animation.FuncAnimation(fig, update, frames=frames, blit=True, interval=15)
 #ani.save('./fig_output/'+'rotationPoints_animate.gif', writer='PillowWriter', fps=30)
 plt.show()
